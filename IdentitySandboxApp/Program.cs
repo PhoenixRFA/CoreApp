5faa9749f90d1992bd68c232304a1b3e367bd548
phoenix <gralukkr@gmail.com>
2021-01-24 21:39:12 +0300

    Init data in program

--- IdentitySandboxApp/Program.cs
+++ IdentitySandboxApp/Program.cs
@@ -1,10 +1,9 @@
+using IdentitySandboxApp.Models.Identity;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace IdentitySandboxApp
@@ -13,7 +12,12 @@ namespace IdentitySandboxApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            InitData(scope.ServiceProvider).Wait();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,53 @@ namespace IdentitySandboxApp
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static async Task InitData(IServiceProvider services)
+        {
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole<long>>>();
+
+            if (!await roleManager.RoleExistsAsync("admin"))
+            {
+                var role = new IdentityRole<long>("admin");
+                await roleManager.CreateAsync(role);
+            }
+
+            if (!await roleManager.RoleExistsAsync("user"))
+            {
+                var role = new IdentityRole<long>("user");
+                await roleManager.CreateAsync(role);
+            }
+
+            var userManager = services.GetRequiredService<UserManager<User>>();
+
+            var admin = await userManager.FindByNameAsync("admin");
+            if (admin == null)
+            {
+                var user = new User {
+                    DateOfBirth = DateTime.MinValue,
+                    DateOfRegistration = DateTime.Now,
+                    Email = "admin@email.com",
+                    EmailConfirmed = true,
+                    UserName = "admin"
+                };
+
+                await userManager.CreateAsync(user, "qwerty");
+            }
+
+            var someUser = await userManager.FindByNameAsync("someUser");
+            if (someUser == null)
+            {
+                var user = new User
+                {
+                    DateOfBirth = new DateTime(1990, 02, 17),
+                    DateOfRegistration = DateTime.Now,
+                    Email = "someUser@email.com",
+                    EmailConfirmed = true,
+                    UserName = "someUser"
+                };
+
+                await userManager.CreateAsync(user, "qwerty");
+            }
+        }
     }
 }
