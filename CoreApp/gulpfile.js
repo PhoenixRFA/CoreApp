0460dbe1cc6e21f189643856b88541beaf60f3ba
phoenix <gralukkr@gmail.com>
2021-01-27 22:40:00 +0300

    gulp js minify, clean, copyModules

--- CoreApp/gulpfile.js
+++ CoreApp/gulpfile.js
@@ -18,23 +18,33 @@ const gulp = require('gulp'),
     //https://github.com/gulp-community/gulp-concat#readme
     concat = require('gulp-concat'),
     //https://github.com/hparra/gulp-rename
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    //https://github.com/adametry/gulp-eslint
+    eslint = require('gulp-eslint'),
+    //https://github.com/duan602728596/gulp-terser
+    minify = require('gulp-terser'),
+    //https://github.com/sindresorhus/del
+    del = require('del'),
+    //https://github.com/dshemendiuk/gulp-npm-dist
+    npmDist = require('gulp-npm-dist');
 
 const webroot = './wwwroot/';
 const paths = {
-    //js: `${webroot}js/**/*.js`,
+    js: `${webroot}js/**/*.js`,
     //minJs: `${webroot}js/**/*.min.js`,
     //css: `${webroot}css/**/*.css`,
     //minCss: `${webroot}css/**/*.min.css`,
     sass: `${webroot}sass/**/*.scss`,
-    //concatJsDest: `${webroot}js/bundle.min.js`,
+    concatJsDest: `${webroot}js/`,
     concatCssDest: `${webroot}css/`
 };
 
 // Clean assets
-//function clean() {
-//    return del([paths.concatCssDest, paths.concatJsDest]);
-//}
+function clean(cb) {
+    return del([`${webroot}js/appbundle.min.js`, `${webroot}css/styles.min.css`]).then(() => {
+        cb();
+    });
+}
 
 // CSS task
 function css() {
@@ -63,13 +73,30 @@ function css() {
 }
 
 // Lint scripts
-//function scriptsLint() {
-//    return gulp
-//        .src([paths.js])
-//        .pipe(plumber())
-//        .pipe(eslint())
-//        .pipe(eslint.format())
-//        .pipe(eslint.failAfterError());
-//}
+function js() {
+    return gulp
+    .src(paths.js)
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())//outputs the lint results to the console
+    //.pipe(eslint.failAfterError())
+    .pipe(sourcemaps.init())
+    .pipe(concat('appbundle.js'))
+    .pipe(minify())
+    //.pipe(sourcemaps.write('/map'))
+    .pipe(sourcemaps.write())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.concatJsDest));
+}
+
+function copyModules(cb) {
+    del(`${webroot}lib`).then(() => {
+        gulp.src(npmDist(), { base: `./node_modules` })
+            .pipe(gulp.dest(`${webroot}lib`)).on('end', cb)
+    }).catch(cb)
+}
 
 module.exports.css = css;
+module.exports.clean = clean;
+module.exports.js = js;
+module.exports.copyModules = copyModules;
---
+++ CoreApp/wwwroot/js/script2.js
@@ -0,0 +1,12 @@
+﻿class myClass {
+    constructor(async_param) {
+        if (typeof async_param === 'undefined') {
+            throw new Error('Cannot be called directly');
+        }
+    }
+
+    static async build() {
+        var async_result = await doSomeAsyncStuff();
+        return new myClass(async_result);
+    }
+}
