e66e50ca24fbc7b806975af29d42cb8f02eef515
phoenix <phoenix@email.com>
2021-01-27 16:53:18 +0300

    gulp scss

--- CoreApp/gulpfile.js
+++ CoreApp/gulpfile.js
@@ -1,59 +1,70 @@
-﻿"use strict"
+﻿'use strict';
 
-//const autoprefixer = require("autoprefixer");
-//const eslint = require("gulp-eslint");
-//const imagemin = require("gulp-imagemin");
-const cssnano = require("cssnano");
-const del = require("del");
-const gulp = require("gulp");
-const newer = require("gulp-newer");
-const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
+const gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    cleanCss = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    shorthand = require('gulp-shorthand'),
+    autoprefixer = require('gulp-autoprefixer'),
+    stylelint = require('gulp-stylelint'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
 
-//const
-//    gulp = require("gulp"),
-//    concat = require("gulp-concat"),
-//    cssmin = require("gulp-cssmin"),
-//    uglify = require("gulp-uglify"),
-//    sass = require("gulp-sass"),
-//    rimraf = require("rimraf");
-
+const webroot = './wwwroot/';
 const paths = {
-    webroot: './webroot/',
-    js: `${paths.webroot}js/**/*.js`,
-    minJs: `${paths.webroot}js/**/*.min.js`,
-    css: `${paths.webroot}css/**/*.css`,
-    minCss: `${paths.webroot}css/**/*.min.css`,
-    scss: `${paths.webroot}sass/**/*.scss`,
-    concatJsDest: `${paths.webroot}js/bundle.min.js`,
-    concatCssDest: `${paths.webroot}css/`,//style.min.css
+    //js: `${webroot}js/**/*.js`,
+    //minJs: `${webroot}js/**/*.min.js`,
+    //css: `${webroot}css/**/*.css`,
+    //minCss: `${webroot}css/**/*.min.css`,
+    sass: `${webroot}sass/**/*.scss`,
+    //concatJsDest: `${webroot}js/bundle.min.js`,
+    concatCssDest: `${webroot}css/`
 };
 
 // Clean assets
-function clean() {
-    return del([paths.concatCssDest, paths.concatJsDest]);
-}
+//function clean() {
+//    return del([paths.concatCssDest, paths.concatJsDest]);
+//}
 
 // CSS task
 function css() {
     return gulp
-        .src(paths.sass)
-        .pipe(plumber())
-        .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(gulp.dest(paths.concatCssDest))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(postcss([cssnano()]))
-        .pipe(gulp.dest(paths.concatCssDest));
+    .src(paths.sass)
+    .pipe(plumber())
+    .pipe(stylelint({
+        failAfterError: false,
+        reporters: [
+            {
+                formatter: 'string',
+                console: true
+            }
+        ]
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(concat('styles.css'))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(shorthand())
+    .pipe(cleanCss({
+        debug: true,
+        compatibility: '*'
+    }, details => {
+        console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`);
+    }))
+    .pipe(sourcemaps.write('/map'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.concatCssDest));
 }
 
 // Lint scripts
-function scriptsLint() {
-    return gulp
-        .src([paths.js])
-        .pipe(plumber())
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-}
+//function scriptsLint() {
+//    return gulp
+//        .src([paths.js])
+//        .pipe(plumber())
+//        .pipe(eslint())
+//        .pipe(eslint.format())
+//        .pipe(eslint.failAfterError());
+//}
+
+module.exports.css = css;
