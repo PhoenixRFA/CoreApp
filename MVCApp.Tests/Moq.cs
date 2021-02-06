21f46090faa3513d60f8229da7c62c33b61f0c25
phoenix <phoenix@email.com>
2021-02-06 16:09:34 +0300

    Some mocking

--- MVCApp.Tests/Moq.cs
+++ MVCApp.Tests/Moq.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using MVCApp.Services;
 using Xunit;
+using Range = Moq.Range;
 
 namespace MVCApp.Tests
 {
@@ -47,6 +50,9 @@ namespace MVCApp.Tests
             Assert.True(mock.Object.DoSomething("ping"));
             Assert.False(mock.Object.DoSomething("pong"));
 
+            var stub = Mock.Of<IFoo>(x => x.DoSomething("ping") == true);
+            Assert.True(stub.DoSomething("ping"));
+
             //out
             string outString = "ack";
             //TryParse will return true, and the out argument will return "ack", lazy evaluated
@@ -73,11 +79,11 @@ namespace MVCApp.Tests
 
             //throwing when invoked with specific parameters
             mock.Setup(foo => foo.DoSomething("reset")).Throws<InvalidOperationException>();
-            mock.Setup(foo => foo.DoSomething("")).Throws(new ArgumentException("command"));
+            mock.Setup(foo => foo.DoSomething("")).Throws(new ArgumentException("", "command"));
             Assert.Throws<InvalidOperationException>(()=>mock.Object.DoSomething("reset"));
-            //Assert.Throws<ArgumentException>("message",() => mock.Object.DoSomething(""));
+            Assert.Throws<ArgumentException>("command",() => mock.Object.DoSomething(""));
 
-            // lazy evaluating return value
+            //lazy evaluating return value
             int count = 1;
             mock.Setup(foo => foo.GetCount()).Returns(() => count);
             Assert.Equal(1, mock.Object.GetCount());
@@ -91,6 +97,52 @@ namespace MVCApp.Tests
 
             var stub2 = Mock.Of<IRequestStoreService>(x => x.Get(It.IsAny<string>()) == "foo");
             Assert.Equal("foo", stub2.Get(""));
+
+
+            //any value
+            mock.Setup(foo => foo.DoSomething(It.IsAny<string>())).Returns(true);
+            Assert.True(mock.Object.DoSomething("asd"));
+
+            //any value passed in a `ref` parameter (requires Moq 4.8 or later):
+            mock.Setup(foo => foo.Submit(ref It.Ref<Bar>.IsAny)).Returns(true);
+            Assert.True(mock.Object.Submit(ref It.Ref<Bar>.IsAny));
+
+            //matching Func<int>, lazy evaluated
+            mock.Setup(foo => foo.Add(It.Is<int>(i => i % 2 == 0))).Returns(true);
+            Assert.True(mock.Object.Add(2));
+            Assert.False(mock.Object.Add(1));
+
+            //matching ranges
+            mock.Setup(foo => foo.Add(It.IsInRange<int>(0, 10, Range.Inclusive))).Returns(true);
+            Assert.True(mock.Object.Add(1));
+            Assert.False(mock.Object.Add(11));
+
+
+            mock.Setup(foo => foo.Name).Returns("bar");
+            Assert.Equal("bar", mock.Object.Name);
+
+            //auto-mocking hierarchies (a.k.a. recursive mocks)
+            mock.Setup(foo => foo.Bar.Baz.Name).Returns("baz");
+            Assert.Equal("baz", mock.Object.Bar.Baz.Name);
+
+            //expects an invocation to set the value to "foo"
+            mock.SetupSet(foo => foo.Name = "foo");
+            mock.Object.Name = "foo";
+            //verify the setter directly
+            mock.VerifySet(foo => foo.Name = "foo");
+
+
+            //alternatively, provide a default value for the stubbed property
+            mock.SetupProperty(f => f.Name, "foo");
+
+            //Now you can do:
+            IFoo foo = mock.Object;
+            //Initial value was stored
+            Assert.Equal("foo", foo.Name);
+
+            //New value set which changes the initial value
+            foo.Name = "bar";
+            Assert.Equal("bar", foo.Name);
         }
     }
 }
