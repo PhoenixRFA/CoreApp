63a371e85444c90fc2fc68170c071d2c3bf29d06
phoenix <gralukkr@gmail.com>
2021-02-22 21:02:46 +0300

    fcm

---
+++ MVCApp/wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: '366688769796'
+});
+
+const messaging = firebase.messaging();
---
+++ MVCApp/wwwroot/js/firebase_subscribe.js
@@ -0,0 +1,66 @@
+﻿firebase.initializeApp({
+    messagingSenderId: '366688769796'
+});
+let messaging;
+
+if ('Notification' in window) {
+    messaging = firebase.messaging();
+
+    if (Notification.permission === 'granted') {
+        subscribe();
+    }
+
+    $('#subscribe').on('click', function () {
+        subscribe();
+    });
+}
+
+function subscribe() {
+    messaging.requestPermission()
+        .then(function () {
+            messaging.getToken()
+                .then(function (currentToken) {
+                    console.log({ currentToken });
+
+                    if (currentToken) {
+                        sendTokenToServer(currentToken);
+                    } else {
+                        console.warn('Error on token receiving');
+                        setTokenSentToServer(false);
+                    }
+                })
+                .catch(function (err) {
+                    console.warn('Error on token receiving', err);
+                    setTokenSentToServer(false);
+                });
+        })
+        .catch(function (err) {
+            console.warn('Notification access isnt granted', err);
+        });
+}
+
+function sendTokenToServer(currentToken) {
+    if (!isTokenSentToServer(currentToken)) {
+        console.log('Sending token to server');
+
+        const url = '';
+        //$.post(url, {
+        //    token: currentToken
+        //});
+
+        setTokenSentToServer(currentToken);
+    } else {
+        console.log('Token already sent to the server');
+    }
+}
+
+function isTokenSentToServer(currentToken) {
+    return window.localStorage.getItem('sentFirebaseMessagingToken') == currentToken;
+}
+
+function setTokenSentToServer(currentToken) {
+    window.localStorage.setItem(
+        'sentFirebaseMessagingToken',
+        currentToken ? currentToken : ''
+    );
+}
